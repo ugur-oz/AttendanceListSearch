7a6db86f2ee0abd810e103acc3b9835e9e6d4545
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-04-01 18:03:38 +0200

    Calisiyor

--- src/main/java/com/ugur/Controller.java
+++ src/main/java/com/ugur/Controller.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping("/login")
+    @GetMapping("/")
     public String getstart(){
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
 
         return "login";
     }
