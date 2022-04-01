fa453fae372b26e0e82d91c950a4029dbb3a67ef
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-04-01 17:49:21 +0200

    Posgres-4 db

--- src/main/resources/db/migration/V1_0_3_initial-db.sql
+++ src/main/resources/db/migration/V1_0_3_initial-db.sql
@@ -1,1 +1,1 @@
-SELECT setval(pg_get_serial_sequence('USERS', 'USERNAME'), coalesce(max(id)+1, 1), false) FROM users;
+SELECT setval(pg_get_serial_sequence('USERS', 'USERNAME'), coalesce(max(id)+1, 1), false) FROM USERS;
