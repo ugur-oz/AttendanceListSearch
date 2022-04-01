064384c896c50649944a6bff87e65f4d9e040f3b
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-04-01 17:59:28 +0200

    Posgres-6 db

--- src/main/java/com/ugur/Controller.java
+++ src/main/java/com/ugur/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping("/")
+    @GetMapping("/login")
     public String getstart(){
 
         return "login";
