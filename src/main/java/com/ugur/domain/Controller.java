1a73514a6c1863b06fb3dc1fdf0c616bc3639965
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-04-26 10:07:10 +0200

    With Dummy Datas

--- src/main/java/com/ugur/domain/Controller.java
+++ src/main/java/com/ugur/domain/Controller.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 
 @org.springframework.stereotype.Controller
