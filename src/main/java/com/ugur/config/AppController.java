09082b065604d95feda338fce565b81e105992d0
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-05-05 17:37:48 +0200

    FunktioniertwithAttendance

--- src/main/java/com/ugur/config/AppController.java
+++ src/main/java/com/ugur/config/AppController.java
@@ -100,7 +100,6 @@ public class AppController {
         anwesenheit.setSignature(anwesenheitForm.getSignature());
         anwesenheit.setUser(userRepository.getById(anwesenheitForm.getUserid()));
 
-
         anwesenheitRepository.save(anwesenheit);
         return "redirect:logout";
     }
