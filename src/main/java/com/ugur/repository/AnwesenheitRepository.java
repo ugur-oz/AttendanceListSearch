7648658832b81c56d8ee940221f3a4e0f767b2ec
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-05-09 21:48:12 +0200

    SortBy+Searchv2+

--- src/main/java/com/ugur/repository/AnwesenheitRepository.java
+++ src/main/java/com/ugur/repository/AnwesenheitRepository.java
@@ -17,5 +17,4 @@ public interface AnwesenheitRepository extends JpaRepository<Anwesenheit, Long> 
     @Query(value = "SELECT * FROM Users u LEFT JOIN Attendance s ON u.user_id = s.user_id  where u.username  like %:keyword% or s.anwesenheit_id like %:keyword% or s.user_id like %:keyword% or s.datum like %:keyword% or s.signature like %:keyword%", nativeQuery = true)
     List<Anwesenheit> findByKeyword(@Param("keyword") String keyword);
 
-
 }
